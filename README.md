b2c3d9d156ce051da04b16b8fbe04398b6ef2abf
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-07-22 22:09:52 -0300

    axios

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 #  Modern React with Redux 
 Stephen Grider
+
+### Semantic ui cdn 
+https://semantic-ui.com/
+https://cdnjs.com/libraries/semantic-ui
+npm install semantic-ui-css --save-dev
+
+### Faker js
+npm install --save faker
+
+### unsplash
+https://unsplash.com/developers
+
+### Axios
+npm i axios
--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+
+    onSearchSubmit (term) {
+        console.log(term);
+        axios.get( 'https://api.unsplash.com/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID Uusl5F1Q6rpv7OKevgUKzNH4uSB8pNXa4NvW8gGDAhI' 
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmit={ this.onSearchSubmit } />
+            </div>
+        )
+    }
 }
 
 export default App;
