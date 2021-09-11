d790f098fd80bba5b033992a39d3f8534300770a
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-09-11 20:53:48 -0300

    dropdown menu

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 
 const items = [
@@ -18,10 +19,35 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'Red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'Green'
+    },
+    {
+        label: 'The Shade of Blue',
+        value: 'Blue'
+    },
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div>
-            <Search />
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+            {showDropdown &&
+                <Dropdown 
+                    selected={selected} 
+                    onSelectedChange={setSelected}
+                    options={options} 
+                />
+            }
         </div>
     );
 }
