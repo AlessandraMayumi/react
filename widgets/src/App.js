be8552d72e7ed448b739a260436181e6e6000309
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-09-13 07:53:13 -0300

    links

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Accordion from './components/Accordion';
-import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Header from './components/Header';
+import Search from './components/Search';
 import Translate from './components/Translate';
 import Route from './components/Route';
 
@@ -41,6 +42,7 @@ export default () => {
 
     return (
         <div>
+            <Header />
             <Route path="/">
                 <Accordion items={items} />
             </Route>
