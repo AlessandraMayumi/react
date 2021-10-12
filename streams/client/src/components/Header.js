c80f0e96187bdf1a7012ec1a8e706a1934eb6df6
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-10-12 08:37:16 -0300

    header

---
+++ streams/client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <div className="ui secondary pointing menu">
+            <Link to="/" className="item">Streamy</Link>
+            <div className="right menu">
+                Header
+            </div>
+        </div>
+    )
+}
+
+export default Header;
