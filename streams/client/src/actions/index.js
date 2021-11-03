ee45d5f9bf29371de047982d367b9225e71c4afa
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-11-03 19:45:21 -0300

    edit delete button

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,3 @@
-import { formValues } from 'redux-form';
 import streams from '../apis/streams';
 import { 
     SIGN_IN, 
@@ -24,8 +23,9 @@ export const signOut = userId => {
     };
 };
 
-export const createStream = formValues => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
