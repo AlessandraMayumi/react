20866f432189e1f896cc4b5036d2fff9e5b46d02
AlessandraMayumi <alessandra_mms@hotmail.com>
2022-01-15 21:05:10 -0300

    modal route

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -2,23 +2,28 @@ import React from "react";
 import Modal from '../Modal';
 import history from "../../history";
 
-const StreamDelete = () => {
-    const actions = (
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>     
-    );
-    return (
-    <div>
-        StreamDelete
-        <Modal 
-            title="Delete Stream"
-            content="Are you sure you want delete this stream?"
-            actions={actions}
-            onDismiss={() => history.pushState('/')}
-        />
-    </div>);
+class StreamDelete extends React.Component {
+    renderActions() {
+        return (
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>     
+        );
+    }
+    
+    render() {
+        return (
+            <div>
+                StreamDelete
+                <Modal 
+                    title="Delete Stream"
+                    content="Are you sure you want delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div>);
+    }
 }
 
 export default StreamDelete;
